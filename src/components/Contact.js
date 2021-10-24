bc782e1f1598d2ac8520ff9c80c588370bd3dbed
Riley Lum <rileylum@adam.com.au>
2021-10-24 10:47:24 +1030

    add form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,16 +3,39 @@ import React, {useState} from "react";
 function Contact() {
 
     const [fields, setFields] = useState({name:"",email:"",message:""});
+    const [errors, setErrors] = useState({name:"",email:"",message:""});
 
     const handleSubmit = (e) => {
         e.preventDefault();
     }
 
     const handleChange = (e) => {
-        console.log(e);
         let newFields = fields;
         newFields[e.target.id] = e.target.value;
         setFields({...newFields});
+    }
+
+    const formValidate = (e) => {
+
+        let field = e.target.id;
+        console.log(field)
+        let newErrors = errors;
+        if (!e.target.value) {
+            newErrors[e.target.id] = `${field} cannot be empty`
+        }
+        if (field === "email") {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (!re.test(String(e.target.value).toLowerCase())) {
+                newErrors["email"] = "email must be valid"
+            }
+        }
+        setErrors({...newErrors});
+    }
+
+    const clearValidate = (e) => {
+        let newErrors = errors;
+        newErrors[e.target.id] = "";
+        setErrors({...newErrors});
     }
 
     return (
@@ -21,16 +44,19 @@ function Contact() {
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" value={fields.name} onChange={handleChange} className="form-control" id="name"/>
+                    <input type="text" value={fields.name} onChange={handleChange} onBlur={formValidate} onFocus={clearValidate} className="form-control" id="name"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email Address</label>
-                    <input type="email" value={fields.email} onChange={handleChange} className="form-control" id="email"/>
+                    <input type="email" value={fields.email} onChange={handleChange} onBlur={formValidate} onFocus={clearValidate} className="form-control" id="email"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="message" className="form-label">Message</label>
-                    <textarea value={fields.message} onChange={handleChange} className="form-control" id="message" rows="3"></textarea>
+                    <textarea value={fields.message} onChange={handleChange}  onBlur={formValidate} onFocus={clearValidate} className="form-control" id="message" rows="3"></textarea>
                 </div>
+                {errors && (<p className="d-block">{`${errors.name}`}</p>)}
+                {errors && (<p className="d-block">{`${errors.email}`}</p>)}
+                {errors && (<p className="d-block">{`${errors.message}`}</p>)}
                 <button type="submit" className="btn btn-primary mb-3">Submit</button>
             </form>
         </section>
